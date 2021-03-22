3c3090e4b5c255c875ee2556892604ffe90a272f
Adityasingh-01 <adi5091659@gmail.com>
2021-03-22 15:41:41 +0530

    Update Customer.java
    Promoting getter and setter

--- demo/src/main/java/com/service/demo/vo/Customer.java
+++ demo/src/main/java/com/service/demo/vo/Customer.java
@@ -1,4 +1,24 @@
 package com.service.demo.vo;
 
 public class Customer {
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
